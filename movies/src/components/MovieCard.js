52630a5f5aa650b2fd9d7dccfd7d21015535e2d9
Olga <olganedelcuam@gmail.com>
2023-03-08 14:21:46 +0100

    trying to solve the fetching issue

--- movies/src/components/MovieCard.js
+++ movies/src/components/MovieCard.js
@@ -1,24 +1,41 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+
 
 const MovieCard = () => {
-    const [movies, setMovies] = useState([]);
-   
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState([]);
+
   useEffect(() => {
-    fetch("http://www.omdbapi.com/?s=Batman&page=2&apikey=350bc702")
-    .then((res) => res.json())
-    .then((data) => setMovies(data));
-}, []);
+    const fetchData = async () => {
+      const response = await fetch(
+        "http://www.omdbapi.com/?s=Batman&page=2&apikey=350bc702"
+      );
+      const json = await response.json();
+      setMovies(json.Search);
+      setIsLoading(false);
+      // console.log(json.Search);
+    };
 
+    setTimeout(() => {
+      fetchData();
+    }, 2000);
+  }, []);
 
-    return (
+  return (
     <div>
-        {movies.map(((movie) => (
-            <div>
-                <h2>{movie.title} - {movie.year}</h2>
-            </div>
-        )))}
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        movies.map((movie) => (
+          <div>
+            <h2>
+              {movie.title} - {movie.year}
+            </h2>
+          </div>
+        ))
+      )}
     </div>
-    )
-}
+  );
+};
 
 export default MovieCard;
