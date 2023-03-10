96bf76d8346f3347755fc4820eb7db41f749ed7f
Olga <olganedelcuam@gmail.com>
2023-03-10 15:40:11 +0100

    fetching data with useEffect hook

--- movies/src/components/MovieCard.js
+++ movies/src/components/MovieCard.js
@@ -3,23 +3,33 @@ import React, { useState, useEffect } from "react";
 const MovieCard = () => {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-
+  /*
   const fetchData = async () => {
+    setIsLoading(true);
     const response = await fetch(
       "http://www.omdbapi.com/?s=Batman&page=2&apikey=350bc702"
     );
     const json = await response.json();
     setMovies(json.Search);
+    setIsLoading(false);
     // console.log(json.Search);
     console.log(json.Search.Title);
-  };
+  }; */
 
   useEffect(() => {
     setIsLoading(true);
+    fetch("http://www.omdbapi.com/?s=Batman&page=2&apikey=350bc702")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => setMovies(data.Search));
+      setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
     setTimeout(() => {
       fetchData();
     }, 2000);
-    setIsLoading(false);
   }, []);
 
   return (
