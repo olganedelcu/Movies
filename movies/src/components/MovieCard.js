754c443502b2d5b9773f26614aa60ef549c4c7cc
Olga <olganedelcuam@gmail.com>
2023-03-10 16:06:10 +0100

    error handling with catch-try block

--- movies/src/components/MovieCard.js
+++ movies/src/components/MovieCard.js
@@ -3,29 +3,20 @@ import React, { useState, useEffect } from "react";
 const MovieCard = () => {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
-  /*
+
   const fetchData = async () => {
-    setIsLoading(true);
-    const response = await fetch(
-      "http://www.omdbapi.com/?s=Batman&page=2&apikey=350bc702"
-    );
-    const json = await response.json();
-    setMovies(json.Search);
-    setIsLoading(false);
-    // console.log(json.Search);
-    console.log(json.Search.Title);
-  }; */
-
-  useEffect(() => {
-    setIsLoading(true);
-    fetch("http://www.omdbapi.com/?s=Batman&page=2&apikey=350bc702")
-      .then((res) => {
-        return res.json();
-      })
-      .then((data) => setMovies(data.Search));
-      setIsLoading(false);
-  }, []);
-
+    try {
+        setIsLoading(true);
+        const response = await fetch("http://www.omdbapi.com/?s=Batman&page=2&apikey=350bc702");
+        const data = await response.json();
+        setMovies(data.Search);
+        setIsLoading(false);
+    }   catch (error) {
+        console.log(error);
+        setIsLoading(false);
+    }
+  };
+  
   useEffect(() => {
     setTimeout(() => {
       fetchData();
