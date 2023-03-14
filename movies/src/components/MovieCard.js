7d35d22c80f2d345263a982700b3e9b81127ab11
Olga <olganedelcuam@gmail.com>
2023-03-14 16:37:30 +0100

    improving performance overall, HTTP request abortion and calling clearTimeout()

--- movies/src/components/MovieCard.js
+++ movies/src/components/MovieCard.js
@@ -4,23 +4,32 @@ const MovieCard = () => {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
 
-  const fetchData = async () => {
+  const fetchData = async (controller) => {
     try {
-        setIsLoading(true);
-        const response = await fetch("http://www.omdbapi.com/?s=Batman&page=2&apikey=350bc702");
-        const data = await response.json();
-        setMovies(data.Search);
-        setIsLoading(false);
-    }   catch (error) {
-        console.log(error);
-        setIsLoading(false);
+      setIsLoading(true);
+      const response = await fetch(
+        "http://www.omdbapi.com/?s=Batman&page=2&apikey=350bc702",
+        { signal: controller.signal }
+      );
+      const data = await response.json();
+      setMovies(data.Search);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
     }
   };
-  
+
   useEffect(() => {
-    setTimeout(() => {
-      fetchData();
+    const controller = new AbortController();
+    const timer = setTimeout(() => {
+      fetchData(controller); // passing controller to our fetchData function
     }, 2000);
+
+    return () => {
+      clearTimeout(timer);
+      controller.abort();
+    };
   }, []);
 
   return (
